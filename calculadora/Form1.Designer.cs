c386961f421a2bde0cd3947475efaeb7f15b9044
Arthur Magalhães <arthurmagalhaes13@hotmail.com>
2021-11-04 23:15:16 -0300

    calculadora pronta

--- calculadora/Form1.Designer.cs
+++ calculadora/Form1.Designer.cs
@@ -41,21 +41,23 @@ namespace calculadora
             this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
-            this.button13 = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
-            this.button15 = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.soma = new System.Windows.Forms.Button();
+            this.subtracao = new System.Windows.Forms.Button();
+            this.multiplicacao = new System.Windows.Forms.Button();
+            this.divisao = new System.Windows.Forms.Button();
+            this.voltar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.BoxTela = new System.Windows.Forms.TextBox();
+            this.display = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(99, 206);
+            this.button2.Location = new System.Drawing.Point(69, 138);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(41, 35);
             this.button2.TabIndex = 1;
@@ -65,7 +67,7 @@ namespace calculadora
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(146, 206);
+            this.button3.Location = new System.Drawing.Point(116, 138);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(41, 35);
             this.button3.TabIndex = 2;
@@ -75,7 +77,7 @@ namespace calculadora
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(52, 165);
+            this.button4.Location = new System.Drawing.Point(22, 97);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(41, 35);
             this.button4.TabIndex = 5;
@@ -85,7 +87,7 @@ namespace calculadora
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(99, 165);
+            this.button5.Location = new System.Drawing.Point(69, 97);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(41, 35);
             this.button5.TabIndex = 4;
@@ -95,7 +97,7 @@ namespace calculadora
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(146, 165);
+            this.button6.Location = new System.Drawing.Point(116, 97);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(41, 35);
             this.button6.TabIndex = 3;
@@ -105,7 +107,7 @@ namespace calculadora
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(52, 124);
+            this.button7.Location = new System.Drawing.Point(22, 56);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(41, 35);
             this.button7.TabIndex = 8;
@@ -115,7 +117,7 @@ namespace calculadora
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(99, 124);
+            this.button8.Location = new System.Drawing.Point(69, 56);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(41, 35);
             this.button8.TabIndex = 7;
@@ -125,7 +127,7 @@ namespace calculadora
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(146, 124);
+            this.button9.Location = new System.Drawing.Point(116, 56);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(41, 35);
             this.button9.TabIndex = 6;
@@ -135,9 +137,9 @@ namespace calculadora
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(99, 247);
+            this.button10.Location = new System.Drawing.Point(22, 179);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(41, 35);
+            this.button10.Size = new System.Drawing.Size(88, 35);
             this.button10.TabIndex = 9;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
@@ -145,7 +147,7 @@ namespace calculadora
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(146, 247);
+            this.button11.Location = new System.Drawing.Point(116, 179);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(41, 35);
             this.button11.TabIndex = 10;
@@ -155,76 +157,77 @@ namespace calculadora
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(193, 42);
+            this.button12.Location = new System.Drawing.Point(210, 56);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(41, 35);
             this.button12.TabIndex = 11;
-            this.button12.Text = "<-";
+            this.button12.Text = "CE";
             this.button12.UseVisualStyleBackColor = true;
             this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
-            // button13
+            // igual
             // 
-            this.button13.Location = new System.Drawing.Point(193, 247);
-            this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(41, 35);
-            this.button13.TabIndex = 15;
-            this.button13.Text = "=";
-            this.button13.UseVisualStyleBackColor = true;
+            this.igual.Location = new System.Drawing.Point(210, 138);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(41, 76);
+            this.igual.TabIndex = 15;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = true;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
             // 
-            // button14
+            // soma
             // 
-            this.button14.Location = new System.Drawing.Point(193, 206);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(41, 35);
-            this.button14.TabIndex = 14;
-            this.button14.Text = "+";
-            this.button14.UseVisualStyleBackColor = true;
-            this.button14.Click += new System.EventHandler(this.button14_Click);
+            this.soma.Location = new System.Drawing.Point(163, 56);
+            this.soma.Name = "soma";
+            this.soma.Size = new System.Drawing.Size(41, 35);
+            this.soma.TabIndex = 14;
+            this.soma.Text = "+";
+            this.soma.UseVisualStyleBackColor = true;
+            this.soma.Click += new System.EventHandler(this.button14_Click);
             // 
-            // button15
+            // subtracao
             // 
-            this.button15.Location = new System.Drawing.Point(193, 165);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(41, 35);
-            this.button15.TabIndex = 13;
-            this.button15.Text = "-";
-            this.button15.UseVisualStyleBackColor = true;
-            this.button15.Click += new System.EventHandler(this.button15_Click);
+            this.subtracao.Location = new System.Drawing.Point(163, 97);
+            this.subtracao.Name = "subtracao";
+            this.subtracao.Size = new System.Drawing.Size(41, 35);
+            this.subtracao.TabIndex = 13;
+            this.subtracao.Text = "-";
+            this.subtracao.UseVisualStyleBackColor = true;
+            this.subtracao.Click += new System.EventHandler(this.button15_Click);
             // 
-            // button16
+            // multiplicacao
             // 
-            this.button16.Location = new System.Drawing.Point(193, 124);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(41, 35);
-            this.button16.TabIndex = 12;
-            this.button16.Text = "*";
-            this.button16.UseVisualStyleBackColor = true;
-            this.button16.Click += new System.EventHandler(this.button16_Click);
+            this.multiplicacao.Location = new System.Drawing.Point(163, 138);
+            this.multiplicacao.Name = "multiplicacao";
+            this.multiplicacao.Size = new System.Drawing.Size(41, 35);
+            this.multiplicacao.TabIndex = 12;
+            this.multiplicacao.Text = "*";
+            this.multiplicacao.UseVisualStyleBackColor = true;
+            this.multiplicacao.Click += new System.EventHandler(this.button16_Click);
             // 
-            // button17
+            // divisao
             // 
-            this.button17.Location = new System.Drawing.Point(193, 83);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(41, 35);
-            this.button17.TabIndex = 16;
-            this.button17.Text = "/";
-            this.button17.UseVisualStyleBackColor = true;
-            this.button17.Click += new System.EventHandler(this.button17_Click);
+            this.divisao.Location = new System.Drawing.Point(163, 179);
+            this.divisao.Name = "divisao";
+            this.divisao.Size = new System.Drawing.Size(41, 35);
+            this.divisao.TabIndex = 16;
+            this.divisao.Text = "/";
+            this.divisao.UseVisualStyleBackColor = true;
+            this.divisao.Click += new System.EventHandler(this.button17_Click);
             // 
-            // button18
+            // voltar
             // 
-            this.button18.Location = new System.Drawing.Point(52, 247);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(41, 35);
-            this.button18.TabIndex = 17;
-            this.button18.Text = "ESC";
-            this.button18.UseVisualStyleBackColor = true;
-            this.button18.Click += new System.EventHandler(this.button18_Click);
+            this.voltar.Location = new System.Drawing.Point(210, 97);
+            this.voltar.Name = "voltar";
+            this.voltar.Size = new System.Drawing.Size(41, 35);
+            this.voltar.TabIndex = 17;
+            this.voltar.Text = "C";
+            this.voltar.UseVisualStyleBackColor = true;
+            this.voltar.Click += new System.EventHandler(this.button18_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(52, 206);
+            this.button1.Location = new System.Drawing.Point(22, 138);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(41, 35);
             this.button1.TabIndex = 19;
@@ -232,49 +235,71 @@ namespace calculadora
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // BoxTela
+            // display
             // 
-            this.BoxTela.Location = new System.Drawing.Point(52, 42);
-            this.BoxTela.Multiline = true;
-            this.BoxTela.Name = "BoxTela";
-            this.BoxTela.Size = new System.Drawing.Size(135, 76);
-            this.BoxTela.TabIndex = 20;
+            this.display.Location = new System.Drawing.Point(22, 12);
+            this.display.Multiline = true;
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(232, 35);
+            this.display.TabIndex = 20;
+            this.display.TextChanged += new System.EventHandler(this.BoxTela_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
             this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox1.Location = new System.Drawing.Point(240, 42);
+            this.textBox1.Location = new System.Drawing.Point(257, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(13, 240);
+            this.textBox1.Size = new System.Drawing.Size(13, 202);
             this.textBox1.TabIndex = 21;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(33, 42);
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Location = new System.Drawing.Point(3, 12);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(13, 240);
+            this.textBox2.Size = new System.Drawing.Size(13, 202);
             this.textBox2.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, -4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 25;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(291, 333);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(277, 228);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.BoxTela);
+            this.Controls.Add(this.display);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button18);
-            this.Controls.Add(this.button17);
-            this.Controls.Add(this.button13);
-            this.Controls.Add(this.button14);
-            this.Controls.Add(this.button15);
-            this.Controls.Add(this.button16);
+            this.Controls.Add(this.voltar);
+            this.Controls.Add(this.divisao);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.soma);
+            this.Controls.Add(this.subtracao);
+            this.Controls.Add(this.multiplicacao);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button10);
@@ -287,8 +312,9 @@ namespace calculadora
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculadora";
             this.TransparencyKey = System.Drawing.Color.White;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -308,16 +334,18 @@ namespace calculadora
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
-        private System.Windows.Forms.Button button13;
-        private System.Windows.Forms.Button button14;
-        private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button igual;
+        private System.Windows.Forms.Button soma;
+        private System.Windows.Forms.Button subtracao;
+        private System.Windows.Forms.Button multiplicacao;
+        private System.Windows.Forms.Button divisao;
+        private System.Windows.Forms.Button voltar;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox BoxTela;
+        private System.Windows.Forms.TextBox display;
         public System.Windows.Forms.TextBox textBox1;
         public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- calculadora/Form1.cs
+++ calculadora/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,14 @@ namespace calculadora
 {
     public partial class Form1 : Form
     {
+
+        double acumula = 0;
+        string operacao = "";
+
         public Form1()
         {
             InitializeComponent();
-            BoxTela.Text = "";
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,115 +30,164 @@ namespace calculadora
 
         private void button10_Click(object sender, EventArgs e) //o
         {
-            button10.Text = "0";
-            BoxTela.Text += "0";
+            display.Text += "0";
         }
 
         private void button9_Click(object sender, EventArgs e) //9
         {
-            button9.Text = "9";
-            BoxTela.Text += "9";
+            display.Text += "9";
         }
 
         private void button8_Click(object sender, EventArgs e) //8
         {
-            button8.Text = "8";
-            BoxTela.Text += "8";
+            display.Text += "8";
         }
 
         private void button7_Click(object sender, EventArgs e) //7
         {
-            button7.Text = "7";
-            BoxTela.Text += "7";
+            display.Text += "7";
         }
 
         private void button6_Click(object sender, EventArgs e) //6
         {
-            button6.Text = "6";
-            BoxTela.Text += "6";
+            display.Text += "6";
         }
 
         private void button5_Click(object sender, EventArgs e) //5
         {
-            button5.Text = "5";
-            BoxTela.Text += "5";
+            display.Text += "5";
         }
 
         private void button4_Click(object sender, EventArgs e) //4
         {
-            button4.Text = "4";
-            BoxTela.Text += "4";
+            display.Text += "4";
         }
 
         private void button3_Click(object sender, EventArgs e) //3
         {
-            button3.Text = "3";
-            BoxTela.Text += "3";
+            display.Text += "3";
         }
 
         private void button2_Click(object sender, EventArgs e) //2
         {
-            button2.Text = "2";
-            BoxTela.Text += "2";
+            display.Text += "2";
         }
 
         private void button1_Click_1(object sender, EventArgs e) //1
         {
-            button1.Text = "1";
-            BoxTela.Text += "1";
+            display.Text += "1";
         }
 
         private void button14_Click(object sender, EventArgs e) // +
         {
-            button14.Text = " + ";
-            BoxTela.Text += " + ";
+            if (operacao == "*" || operacao == "-" || operacao == "/")
+            {
+                operacao = "+";
+            }
+            else
+            {
+                acumula += double.Parse(display.Text);
+                display.Text = "";
+                operacao = "+";
+            }
         }
 
         private void button15_Click(object sender, EventArgs e) // -
         {
-            button15.Text = " - ";
-            BoxTela.Text += " - ";
+            if (operacao == "*" || operacao == "+" || operacao == "/")
+            {
+                operacao = "-";
+            }
+            else
+            {
+                acumula = double.Parse(display.Text);
+                display.Text = "";
+                operacao = "-";
+            }
         }
 
         private void button16_Click(object sender, EventArgs e) // *
         {
-            button16.Text = " * ";
-            BoxTela.Text += " * ";
+            if (operacao == "-" || operacao == "+" || operacao == "/")
+            {
+                operacao = "*";
+            }
+            else
+            {
+                acumula = double.Parse(display.Text);
+                display.Text = "";
+                operacao = "*";
+            }
         }
 
         private void button17_Click(object sender, EventArgs e) // /
         {
-            button17.Text = " / ";
-            BoxTela.Text += " / ";
+            if (operacao == "*" || operacao == "+" || operacao == "-")
+            {
+                operacao = "/";
+            }
+            else
+            {
+                acumula = double.Parse(display.Text);
+                display.Text = "";
+                operacao = "/";
+            }
         }
 
         private void button12_Click(object sender, EventArgs e) // apagar um numero
         {
-            // apagar um número
+            display.Text = "";
+            operacao = "";
         }
 
         private void button18_Click(object sender, EventArgs e) // apagar td
         {
-            // apagar td
+            int x = display.Text.Length - 1;
+            if (x >= 0)
+            {
+                display.Text = display.Text.Substring(0, x);
+            }
         }
 
         private void button11_Click(object sender, EventArgs e) // ponto
         {
-            button11.Text = ".";
-            BoxTela.Text += ".";
+            display.Text += ".";
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e) // tela calculadora
+        private void BoxTela_TextChanged(object sender, EventArgs e)
         {
-            // tela da calculadora
+
         }
 
-        private void operacao (object sender, EventArgs e)
+        private void igual_Click(object sender, EventArgs e)
         {
-            int contas;
-
-
-
-        }
+            if (operacao == "+")
+            {
+                acumula += double.Parse(display.Text);
+                display.Text = acumula.ToString();
+            }
+            else if (operacao == "-")
+            {
+                acumula -= double.Parse(display.Text);
+                display.Text = acumula.ToString();
+            }
+            else if (operacao == "*")
+            {
+                acumula *= double.Parse(display.Text);
+                display.Text = acumula.ToString();
+            }
+            else if (operacao == "/")
+            {
+                if (double.Parse(display.Text) != 0)
+                {
+                    acumula /= double.Parse(display.Text);
+                    display.Text = acumula.ToString();
+                }
+                else
+                {
+                    display.Text = "Dividindo por zero";
+                }
+            }
+            }
     }
 }
